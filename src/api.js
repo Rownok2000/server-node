2a38dfb3cff6313b69943abbec21a6564cef74bc
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2021-06-05 10:45:22 +0000

    servernetlify 2

--- src/api.js
+++ src/api.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const serverless = require ('serverless-http');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+
 
 const app = express();
 const router = express.Router();
 
-var indexRouter = require('./routes/index');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(new cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
  //E' necessario usare per tutte le route il prefisso /.netlify/functions
- app.use('/.netlify/functions/api/index', indexRouter);
+ app.use('/.netlify/functions/api/users', usersRouter);
+ app.use('/.netlify/functions/api/indes', indexRouter);
+
+ //NB!
+ //NON è necessario avviare il server perchè stiamo creando una lambda function
+ /*
+ app.listen(port, () => {//NON FACCIO NULlA })
+*/
 
 module.exports = app;
 module.exports.handler= serverless(app);  //Qui è dove esportiamo le nostre funzioni per netlify
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 const serverless = require ('serverless-http');
 
-
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({
+      user:"pinco pallino"
+  });
 });
 
 module.exports = router;
 //Anche usando un modulo esterno dobbiamo esportare l'oggetto serverless(router)
-module.exports.handler= serverless(router)
+module.exports.handler= serverless(router); 
