74ce63c57a040cb776276d827395f58dca8d697a
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2021-06-05 10:35:35 +0000

    servernetlify 2

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/index');
 
 
  //E' necessario usare per tutte le route il prefisso /.netlify/functions
- app.use('/.netlify/functions/api/users', usersRouter);
+ app.use('/.netlify/functions/api/index', usersRouter);
 
 module.exports = app;
 module.exports.handler= serverless(app);  //Qui è dove esportiamo le nostre funzioni per netlify
