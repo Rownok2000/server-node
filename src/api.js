f0b80733eb0ea29b690a73ae216d7f36ee1f48b0
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2021-06-05 11:06:42 +0000

    servernetlify pls funzia

--- src/api.js
+++ src/api.js
@@ -3,7 +3,6 @@ const serverless = require ('serverless-http');
 
 const app = express();
 
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
