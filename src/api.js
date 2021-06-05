734108f51c99be309712768904fabbe43f2f853f
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2021-06-05 10:34:16 +0000

    servernetlify 2

--- src/api.js
+++ src/api.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const serverless = require ('serverless-http');
-var usersRouter = require('./routes/users');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 const app = express();
 const router = express.Router();
 
+var usersRouter = require('./routes/index');
+
+
  //E' necessario usare per tutte le route il prefisso /.netlify/functions
  app.use('/.netlify/functions/api/users', usersRouter);
-
- //NB!
- //NON è necessario avviare il server perchè stiamo creando una lambda function
- /*
- app.listen(port, () => {//NON FACCIO NULlA })
-*/
 
 module.exports = app;
 module.exports.handler= serverless(app);  //Qui è dove esportiamo le nostre funzioni per netlify
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 const serverless = require ('serverless-http');
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({
-      user:"pinco pallino"
-  });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
