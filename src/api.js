29530811ea109e44024af1325b21cdf99ec22844
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2021-06-05 10:37:34 +0000

    servernetlify 1

--- src/api.js
+++ src/api.js
@@ -7,11 +7,11 @@ var logger = require('morgan');
 const app = express();
 const router = express.Router();
 
-var usersRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 
 
  //E' necessario usare per tutte le route il prefisso /.netlify/functions
- app.use('/.netlify/functions/api/index', usersRouter);
+ app.use('/.netlify/functions/api/index', indexRouter);
 
 module.exports = app;
 module.exports.handler= serverless(app);  //Qui è dove esportiamo le nostre funzioni per netlify
